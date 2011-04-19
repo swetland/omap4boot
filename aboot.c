262315417654dda40ca9eaf7d48cec7eab389af2
Brian Swetland <swetland@google.com>
2011-04-18 17:01:00 -0700

    aboot: operate as either 2ND or MLO
    Use the boot device information provided by the ROM to figure out if
    we're operating from USB download or from eMMC boot.
    
    Signed-off-by: Brian Swetland <swetland@google.com>

--- aboot.c
+++ aboot.c
@@ -39,11 +39,108 @@ struct usb usb;
 
 unsigned cfg_machine_type = 2791;
 
-void aboot(void)
+#define API(n) ( (void*) (*((u32 *) (n))) )
+
+struct read_desc {
+	u32 sector_start;
+	u32 sector_count;
+	void *destination;
+};
+
+struct mem_device {
+	u32 initialized;
+	u8 device_type;
+	u8 trials_count;
+	u32 xip_device;
+	u16 search_size;
+	u32 base_address;
+	u16 hs_toc_mask;
+	u16 gp_toc_mask;
+	void *device_data;
+	u16 *boot_options;
+};
+
+struct mem_driver {
+	int (*init)(struct mem_device *md);
+	int (*read)(struct mem_device *md, struct read_desc *rd);
+	int (*configure)(struct mem_device *md, void *config);
+};
+
+int (*rom_get_mem_driver)(struct mem_driver **io, u32 type);
+
+int load_image(unsigned device, unsigned start, unsigned count, void *data)
 {
-	unsigned n;
-	unsigned len;
-	unsigned char *x = (void *) CONFIG_ADDR_DOWNLOAD;
+	struct mem_driver *io = 0;
+	struct mem_device local_md_device, *md = 0;
+	struct read_desc rd;
+	u16 options;
+	int z;
+
+	rom_get_mem_driver = API(0x28404);
+	z = rom_get_mem_driver(&io, device);
+	if (z)
+		return -1;
+
+	md = &local_md_device;
+	memset(md, 0, sizeof(struct mem_device));
+	options = 0; // 1 = init phoenix pmic?
+	md->initialized   = 0;
+	md->device_type   = device;
+	md->xip_device    = 0;
+	md->search_size   = 0;
+	md->base_address  = 0;
+	md->hs_toc_mask   = 0;
+	md->gp_toc_mask   = 0;
+	md->boot_options  = &options;
+	md->device_data   = (void*) 0x80000000;
+	memset(md->device_data, 0, 2500);
+
+	z = io->init(md);
+	if (z)
+		return -1;
+
+	rd.sector_start = start;
+	rd.sector_count = count;
+	rd.destination = data;
+	z = io->read(md, &rd);
+
+	return 0;
+}
+
+int load_from_mmc(unsigned device, unsigned *len)
+{
+	load_image(device, 512, 512, (void*) CONFIG_ADDR_DOWNLOAD);
+	*len = 256 * 1024;
+	return 0;
+}
+
+int load_from_usb(unsigned *_len)
+{
+	unsigned len, n;
+	enable_irqs();
+
+	if (usb_open(&usb))
+		return -1;
+
+	usb_queue_read(&usb, &len, 4);
+	usb_write(&usb, &MSG, 4);
+	n = usb_wait_read(&usb);
+	if (n)
+		return -1;
+
+	if (usb_read(&usb, (void*) CONFIG_ADDR_DOWNLOAD, len))
+		return -1;
+
+	usb_close(&usb);
+
+	disable_irqs();
+	*_len = len;
+	return 0;
+}
+
+void aboot(unsigned *info)
+{
+	unsigned bootdevice, n, len;
 
 	board_mux_init();
 	sdelay(100);
@@ -59,30 +156,32 @@ void aboot(void)
 	serial_init();
 	serial_puts("\n[ aboot second-stage loader ]\n\n");
 
-	enable_irqs();
+	if (info) {
+		bootdevice = info[2] & 0xFF;
+	} else {
+		bootdevice = 0x45;
+	}
 
-	if (usb_open(&usb))
-		goto fail;
+	switch (bootdevice) {
+	case 0x45: /* USB */
+		serial_puts("boot device: USB\n\n");
+		n = load_from_usb(&len);
+		break;
+	case 0x05:
+	case 0x06:
+		serial_puts("boot device: MMC\n\n");
+		n = load_from_mmc(bootdevice, &len);
+		break;
+	default:
+		serial_puts("boot device: unknown\n");
+		for (;;) ;
+	}
 
-	usb_queue_read(&usb, &len, 4);
-	usb_write(&usb, &MSG, 4);
-	n = usb_wait_read(&usb);
-	if (n)
-		goto fail;
-
-	if (usb_read(&usb, (void*) CONFIG_ADDR_DOWNLOAD, len))
-		goto fail;
-
-	usb_close(&usb);
-
-	disable_irqs();
-
-	boot_image(cfg_machine_type, CONFIG_ADDR_DOWNLOAD, len);
-	serial_puts("invalid image\n");
-	for (;;) ;
-
-fail:
-	serial_puts("io error\n");
-	for (;;) ;
+	if (n) {
+		serial_puts("io error\n");
+	} else {
+		boot_image(cfg_machine_type, CONFIG_ADDR_DOWNLOAD, len);
+		serial_puts("invalid image\n");
+	}
 }
 
